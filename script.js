d952988ba5523d2d034a33cb4cfe46d0d5d77619
Shaheer Ahmed <138614123+sherubar@users.noreply.github.com>
2023-08-11 22:17:10 +0400

    Add function for playerChoice
    Added a function which prompts the user to choose from
    rock, paper or scissors and the answer will be checked with
    the value from computerChoice later on in order to declare a winner.

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ function getComputerChoice(){
     const CHOICES = ['rock', 'paper', 'scissors'];
     computerChoice = CHOICES[Math.floor(Math.random()*CHOICES.length)];
     return computerChoice
+};
+
+function getPlayerChoice(){
+    playerChoice = (prompt("Rock, paper, scissors?: ")).toLowerCase()
+    return playerChoice
 }
 
